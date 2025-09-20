a760f5290e0615b5bfb36add6883d640f56099cf
Muscular-Cat <teage.blanton@gmail.com>
2025-09-20 14:07:47 -0500

    Create Lesson 1 Assignment.py

---
+++ Lesson 1 Assignment.py
@@ -0,0 +1,11 @@
+“test”
+myName = “teage”; myName
+myName = teage; myName
+2 = 4
+1 + 999
+1000 – 1
+999 / 3
+333 * 3
+(3 * 333) == (9 * 111)
+print(Completed by, Teage Blanton)
+print(“Completed by, Teage Blanton”)
